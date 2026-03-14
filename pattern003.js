e1665b5f110b7a9e1109699a579d1b3a8e388526
shivendesai0907 <shivendesai0907@gmail.com>
2026-03-14 11:52:20 +0530

    Update pattern003.js

--- pattern003.js
+++ pattern003.js
@@ -1,9 +1,1 @@
-const ROWS = 6;
-for (let i = 1; i <= ROWS; i++) {
-let row = "";
-for (let j = 1; j <= i; j++) {
-const digit = (i + j) % 2 === 0 ? 1 : 0;
-row += (j === 1 ? "" : " ") + digit;
-  }
-console.log(row);
-}
+
